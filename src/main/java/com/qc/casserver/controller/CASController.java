652d4c1c307a8d3255d7b526bc120ff0d7c3eedd
qc <1057117849@qq.com>
2023-06-17 23:48:26 +0800

    修改了CAS接口

--- src/main/java/com/qc/casserver/controller/CASController.java
+++ src/main/java/com/qc/casserver/controller/CASController.java
@@ -1,27 +1,19 @@
 package com.qc.casserver.controller;
 
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.qc.casserver.common.R;
 import com.qc.casserver.pojo.UserResult;
-import com.qc.casserver.pojo.entity.Ticket;
-import com.qc.casserver.pojo.entity.Token;
-import com.qc.casserver.pojo.entity.User;
 import com.qc.casserver.service.AuthService;
 import com.qc.casserver.service.IRedisService;
-import com.qc.casserver.service.OauthService;
-import com.qc.casserver.utils.JWTUtil;
 import com.qc.casserver.utils.RandomName;
-import com.qc.casserver.utils.TicketUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.UUID;
 
 /**
  * 原始CAS认证接口
@@ -36,14 +28,19 @@ public class CASController {
     private AuthService authService;
 
     @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
     private IRedisService iRedisService;
+
     /**
      * 返回字符串yesusername或者no
+     *
      * @param ticket
      * @param service
      * @return
      */
-    @GetMapping(value = "/validate",produces = "text/plain; charset=UTF-8")
+    @GetMapping(value = "/validate", produces = "text/plain; charset=UTF-8")
     public String serverTicketTOUserInfo(@RequestParam("ticket") String ticket,@RequestParam("service") String service){
         if (ticket==null){
             return "no\n";
@@ -53,31 +50,121 @@ public class CASController {
         if (userInfoByST==null){
             return "no\n";
         }
-        if (userInfoByST.getCode().equals(1)){
-            return "yes\n"+userInfoByST.getData().getUsername()+"\n";
+        if (userInfoByST.getCode().equals(1)) {
+            return "yes\n" + userInfoByST.getData().getUsername() + "\n";
         }
         return "no\n";
     }
+
     @GetMapping("/login")
-    public void casLogin(HttpServletResponse response, @RequestParam("service")String service) throws IOException {
-        response.sendRedirect("http://10.15.245.1:55554/?service="+service);
+    public void casLogin(HttpServletResponse response, @RequestParam("service") String service) throws IOException {
+        response.sendRedirect("http://10.15.247.254:55554/?service=" + service);
     }
 
-    /**
-     * serviceValidate
-     * 暂时不支持proxyValidate
-     */
-    @GetMapping(value = "/serviceValidate",produces = MediaType.APPLICATION_XML_VALUE)
-    public String serviceValidate(@RequestParam("ticket") String ticket,@RequestParam("service") String service,String pgtUrl){
-        if (ticket==null){
+    @GetMapping(value = "/serviceValidate", produces = MediaType.APPLICATION_XML_VALUE)
+    public String serviceValidate(HttpServletResponse response, String ticket, String service, String pgtUrl, String format) {
+        if (ticket == null) {
             return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
                     "    <cas:authenticationFailure code=\"INVALID_REQUEST\">\n" +
                     "        Ticket or service parameter was missing in the request.\n" +
                     "    </cas:authenticationFailure>\n" +
                     "</cas:serviceResponse>";
         }
-        R<UserResult> userInfoByST = authService.getUserInfoByST(ticket,service);
-        if (userInfoByST==null){
+        return this.CasP3ServiceAndProxyValidate(response, ticket, service, pgtUrl, format);
+
+    }
+
+    //proxyValidate
+    @GetMapping(value = "/proxyValidate", produces = MediaType.APPLICATION_XML_VALUE)
+    public String proxyValidate(HttpServletResponse response, String ticket, String service, String pgtUrl, String format) {
+        if (ticket == null) {
+            return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
+                    "    <cas:authenticationFailure code=\"INVALID_REQUEST\">\n" +
+                    "        Ticket or service parameter was missing in the request.\n" +
+                    "    </cas:authenticationFailure>\n" +
+                    "</cas:serviceResponse>";
+        }
+        return this.CasP3ServiceAndProxyValidate(response, ticket, service, pgtUrl, format);
+    }
+
+    @GetMapping(value = "/proxy", produces = MediaType.APPLICATION_XML_VALUE)
+    public String proxyValidate(String pgt, String targetService, String format) {
+        if (StringUtils.isEmpty(pgt) || StringUtils.isEmpty(targetService)) {
+            return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
+                    "    <cas:authenticationFailure code=\"INVALID_REQUEST\">\n" +
+                    "        Ticket or service parameter was missing in the request.\n" +
+                    "    </cas:authenticationFailure>\n" +
+                    "</cas:serviceResponse>";
+        }
+        String value = iRedisService.getValue(pgt);
+        if (StringUtils.isEmpty(value)) {
+            return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
+                    "    <cas:authenticationFailure code=\"INVALID_REQUEST\">\n" +
+                    "        Ticket or service parameter was missing in the request.\n" +
+                    "    </cas:authenticationFailure>\n" +
+                    "</cas:serviceResponse>";
+        }
+        String pgts = value.split("<cas:proxyGrantingTicket>")[1].split("</cas:proxyGrantingTicket>")[0];
+        return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
+                "    <cas:authenticationSuccess>\n" +
+                "        <cas:proxyTicket>" + pgts + "</cas:proxyTicket>\n" +
+                "    </cas:authenticationSuccess>\n" +
+                "</cas:serviceResponse>";
+    }
+
+    @GetMapping(value = "/p3/proxyValidate", produces = MediaType.APPLICATION_XML_VALUE)
+    public String CasP3ServiceAndProxyValidate(HttpServletResponse response, String ticket, String service, String pgtUrl, String format) {
+        if (StringUtils.isEmpty(ticket) || StringUtils.isEmpty(service)) {
+            return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
+                    "    <cas:authenticationFailure code=\"INVALID_REQUEST\">\n" +
+                    "        Ticket or service parameter was missing in the request.\n" +
+                    "    </cas:authenticationFailure>\n" +
+                    "</cas:serviceResponse>";
+        }
+        R<UserResult> userInfoByST = authService.getUserInfoByST(ticket, service);
+        if (userInfoByST == null) {
+            return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
+                    "    <cas:authenticationFailure code=\"INVALID_TICKET\">\n" +
+                    "        Ticket " + ticket + " not recognized.\n" +
+                    "    </cas:authenticationFailure>\n" +
+                    "</cas:serviceResponse>";
+        }
+        if (userInfoByST.getCode().equals(1)) {
+
+            String sa = "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
+                    "    <cas:authenticationSuccess>\n" +
+                    "        <cas:user>" + userInfoByST.getData().getUsername() + "</cas:user>\n" +
+                    "        <cas:proxyGrantingTicket>" + ticket + "</cas:proxyGrantingTicket>\n" +
+                    "        <cas:proxies>" + "</cas:proxies>\n" +
+                    "        <cas:attributes>" + "</cas:attributes>\n" +
+                    "    </cas:authenticationSuccess>\n" +
+                    "</cas:serviceResponse>";
+            if (StringUtils.isNotEmpty(pgtUrl)) {
+                String pgt = RandomName.getUUID();
+                iRedisService.setWithTime(pgt, sa, 3600 * 2L);
+                String pgtUrlas = pgtUrl + "?pgtId=" + pgt + "&pgtIou=" + ticket;
+                restTemplate.getForObject(pgtUrlas, String.class);
+            }
+            return sa;
+        }
+        return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
+                "    <cas:authenticationFailure code=\"INVALID_TICKET\">\n" +
+                "        Ticket " + ticket + " not recognized.\n" +
+                "    </cas:authenticationFailure>\n" +
+                "</cas:serviceResponse>";
+    }
+
+    @GetMapping(value = "/p3/serviceValidate", produces = MediaType.APPLICATION_XML_VALUE)
+    public String CasP3ServiceAndProxyValidates(HttpServletResponse response, String ticket, String service, String pgtUrl, String format) {
+        if (StringUtils.isEmpty(ticket) || StringUtils.isEmpty(service)) {
+            return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
+                    "    <cas:authenticationFailure code=\"INVALID_REQUEST\">\n" +
+                    "        Ticket or service parameter was missing in the request.\n" +
+                    "    </cas:authenticationFailure>\n" +
+                    "</cas:serviceResponse>";
+        }
+        R<UserResult> userInfoByST = authService.getUserInfoByST(ticket, service);
+        if (userInfoByST == null) {
             return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
                     "    <cas:authenticationFailure code=\"INVALID_TICKET\">\n" +
                     "        Ticket "+ticket+" not recognized.\n" +
@@ -85,61 +172,22 @@ public class CASController {
                     "</cas:serviceResponse>";
         }
         if (userInfoByST.getCode().equals(1)) {
-            if (StringUtils.isNotEmpty(pgtUrl)) {
-                //生成pgt
-                //传统CAS登录
-                String pgt = RandomName.getUUID();
-                //15过期的st,防止网络缓慢
-                iRedisService.setTicket(pgt,userInfoByST.getData().getId());
-                return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
-                        "    <cas:authenticationSuccess>\n" +
-                        "        <cas:user>" + userInfoByST.getData().getUsername() + "</cas:user>\n" +
-                        "        <cas:proxyGrantingTicket>" + pgt + "</cas:proxyGrantingTicket>\n" +
-                        "    </cas:authenticationSuccess>\n" +
-                        "</cas:serviceResponse>";
 
-            } else {
-
-                return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
-                        "    <cas:authenticationSuccess>\n" +
-                        "        <cas:user>" + userInfoByST.getData().getUsername() + "</cas:user>\n" +
-                        "    </cas:authenticationSuccess>\n" +
-                        "</cas:serviceResponse>";
-
-            }
-        }
-
-        return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
-                "    <cas:authenticationFailure code=\"INVALID_TICKET\">\n" +
-                "        Ticket "+ticket+" not recognized.\n" +
-                "    </cas:authenticationFailure>\n" +
-                "</cas:serviceResponse>";
-    }
-    //proxyValidate
-    @GetMapping(value = "/proxyValidate",produces = MediaType.APPLICATION_XML_VALUE)
-    public String proxyValidate(@RequestParam("ticket") String ticket,@RequestParam("service") String service,String pgtUrl){
-        if (ticket==null){
-            return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
-                    "    <cas:authenticationFailure code=\"INVALID_REQUEST\">\n" +
-                    "        Ticket or service parameter was missing in the request.\n" +
-                    "    </cas:authenticationFailure>\n" +
-                    "</cas:serviceResponse>";
-        }
-        R<UserResult> userInfoByST = authService.getUserInfoByST(ticket,service);
-        if (userInfoByST==null){
-            return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
-                    "    <cas:authenticationFailure code=\"INVALID_TICKET\">\n" +
-                    "        Ticket "+ticket+" not recognized.\n" +
-                    "    </cas:authenticationFailure>\n" +
-                    "</cas:serviceResponse>";
-        }
-        if (userInfoByST.getCode().equals(1)){
-            return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
+            String sa = "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
                     "    <cas:authenticationSuccess>\n" +
-                    "        <cas:user>"+userInfoByST.getData().getUsername()+"</cas:user>\n" +
-                    "        <cas:proxyGrantingTicket>"+ ticket+"</cas:proxyGrantingTicket>\n" +
+                    "        <cas:user>" + userInfoByST.getData().getUsername() + "</cas:user>\n" +
+                    "        <cas:proxyGrantingTicket>" + ticket + "</cas:proxyGrantingTicket>\n" +
+                    "        <cas:proxies>" + "</cas:proxies>\n" +
+                    "        <cas:attributes>" + "</cas:attributes>\n" +
                     "    </cas:authenticationSuccess>\n" +
                     "</cas:serviceResponse>";
+            if (StringUtils.isNotEmpty(pgtUrl)) {
+                String pgt = RandomName.getUUID();
+                iRedisService.setWithTime(pgt, sa, 3600 * 2L);
+                String pgtUrlas = pgtUrl + "?pgtId=" + pgt + "&pgtIou=" + ticket;
+                restTemplate.getForObject(pgtUrlas, String.class);
+            }
+            return sa;
         }
         return "<cas:serviceResponse xmlns:cas='http://www.yale.edu/tp/cas'>\n" +
                 "    <cas:authenticationFailure code=\"INVALID_TICKET\">\n" +
